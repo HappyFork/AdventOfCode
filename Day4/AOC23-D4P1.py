46ef147e24c681dc21a9da35eca5b4044b0cf0d0
Casey <pickles713@hotmail.com>
2023-12-04 17:00:28 -0800

    Start day 4

---
+++ Day4/AOC23-D4P1.py
@@ -0,0 +1,18 @@
+# AOC23-D4P1.py
+# Advent of code 2023, day 4 part 1
+
+FILENAME = "testd4.txt"
+#FILENAME = "inputd4"
+
+total = 0           # Puzzle solution
+
+
+# Open the file
+with open( FILENAME ) as f:
+    lines = f.readlines()
+    for l in lines:
+        pass
+
+
+
+print( total )
